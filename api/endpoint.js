6e70257f9434e7899c4daabc2b6b14149cfcc83b
Andrew Dunkman <adunkman@gmail.com>
2025-10-28 11:59:37 -0400

    Vercel endpoint

--- api/endpoint.js
+++ api/endpoint.js
@@ -1,34 +1,8 @@
-import { statSync, createReadStream } from "fs";
-import { createServer } from "http";
-
-export default createServer((req, res) => {
-  const response_start = Date.now();
-
-  if (req.url === "/") {
-    return sendHtml(res);
-  }
-
-  if (req.url === "/endpoint") {
-    res.writeHead(200, {
-      "Response-Start": response_start,
-      "Response-End": Date.now()
-    });
-    return res.end();
-  }
-
-  res.writeHead(404);
-  return res.end();
-}).listen(process.env.PORT || 3000, () => {
-  console.log("booted");
-});
-
-const sendHtml = (res) => {
-  const file = "./public/index.html";
-  const stat = statSync(file);
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-    "Content-Length": stat.size
-  });
-
-  createReadStream(file).pipe(res);
+export default {
+  fetch() {
+    const response = new Response();
+    response.headers.set("Response-Start", response_start)
+    response.headers.set("Response-End", Date.now())
+    return response;
+  },
 };
