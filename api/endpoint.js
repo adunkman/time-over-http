6cf2c8713936ec74f4b24c05a50fc7bd5856d3a0
Andrew Dunkman <adunkman@gmail.com>
2025-10-28 12:04:31 -0400

    fix: specify response start

--- api/endpoint.js
+++ api/endpoint.js
@@ -1,5 +1,6 @@
 export default {
   fetch() {
+    const response_start = Date.now();
     const response = new Response();
     response.headers.set("Response-Start", response_start)
     response.headers.set("Response-End", Date.now())
